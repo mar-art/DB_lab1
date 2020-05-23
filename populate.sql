aff5a67156cd379863f9e3ee9eae9553865db7d5
mar-art <53080227+mar-art@users.noreply.github.com>
2020-05-23 11:36:36 +0300

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -1,43 +1,43 @@
-INSERT INTO Avocado (id, averageprice, region, month) VALUES ('0','1.33', 'Albany', '12')
-INSERT INTO Avocado (id, averageprice, region, month) VALUES ('1','1.35','	Albany', '12')
-INSERT INTO Avocado (id, averageprice, region, month) VALUES ('2','0.93', 'Atlanta', '12')
-INSERT INTO Avocado (id, averageprice, region, month) VALUES ('3', '1.02', 'Atlanta', '12')
-INSERT INTO Avocado (id, averageprice, region, month) VALUES ('4', '1.00', 'BaltimoreWashington', '11')
-INSERT INTO Avocado (id, averageprice, region, month) VALUES ('5', '0.88', 'Boise', '11')
-INSERT INTO Avocado (id, averageprice, region, month) VALUES ('6', '0.92', 'Boise', '11')
-INSERT INTO Avocado (id, averageprice, region, month) VALUES ('7', '1.11', 'Boise', '11')
-INSERT INTO Avocado (id, averageprice, region, month) VALUES ('8', '1.03', 'Boise', '11')
-INSERT INTO Avocado (id, averageprice, region, month) VALUES ('9', '1.00', 'Boston' '10')
-INSERT INTO Avocado (id, averageprice, region, month) VALUES ('10', '1.33', 'BuffaloRochester' '10')
-INSERT INTO Avocado (id, averageprice, region, month) VALUES ('11', '1.20', 'BuffaloRochester' '10')
-INSERT INTO Avocado (id, averageprice, region, month) VALUES ('12', '1.23', 'BuffaloRochester', '10')
-INSERT INTO Avocado (id, averageprice, region, month) VALUES ('13', '0.99', 'California' '09')
-INSERT INTO Avocado (id, averageprice, region, month) VALUES ('14', '1.21', 'California', '08')
+INSERT INTO Avocado (id, averageprice, region, month) VALUES ('0','1,33', 'Albany', '12');
+INSERT INTO Avocado (id, averageprice, region, month) VALUES ('1','1,35','	Albany', '12');
+INSERT INTO Avocado (id, averageprice, region, month) VALUES ('2','0,93', 'Atlanta', '12');
+INSERT INTO Avocado (id, averageprice, region, month) VALUES ('3', '1,02', 'Atlanta', '12');
+INSERT INTO Avocado (id, averageprice, region, month) VALUES ('4', '1,00', 'BaltimoreWashington', '11');
+INSERT INTO Avocado (id, averageprice, region, month) VALUES ('5', '0,88', 'Boise', '11');
+INSERT INTO Avocado (id, averageprice, region, month) VALUES ('6', '0,92', 'Boise', '11');
+INSERT INTO Avocado (id, averageprice, region, month) VALUES ('7', '1,11', 'Boise', '11');
+INSERT INTO Avocado (id, averageprice, region, month) VALUES ('8', '1,03', 'Boise', '11');
+INSERT INTO Avocado (id, averageprice, region, month) VALUES ('9', '1,00', 'Boston' '10');
+INSERT INTO Avocado (id, averageprice, region, month) VALUES ('10', '1,33', 'BuffaloRochester' '10');
+INSERT INTO Avocado (id, averageprice, region, month) VALUES ('11', '1,20', 'BuffaloRochester' '10');
+INSERT INTO Avocado (id, averageprice, region, month) VALUES ('12', '1,23', 'BuffaloRochester', '10');
+INSERT INTO Avocado (id, averageprice, region, month) VALUES ('13', '0,99', 'California' '09');
+INSERT INTO Avocado (id, averageprice, region, month) VALUES ('14', '1,21', 'California', '08');
 
-INSERT INTO Regions (region) VALUES ('Albany')
-INSERT INTO Regions (region) VALUES ('Atlanta')
-INSERT INTO Regions (region) VALUES ('BaltimoreWashington')
-INSERT INTO Regions (region) VALUES ('Boise')
-INSERT INTO Regions (region) VALUES ('Boston')
-INSERT INTO Regions (region) VALUES ('BuffaloRochester')
-INSERT INTO Regions (region) VALUES ('California')
+INSERT INTO Regions (region) VALUES ('Albany');
+INSERT INTO Regions (region) VALUES ('Atlanta');
+INSERT INTO Regions (region) VALUES ('BaltimoreWashington');
+INSERT INTO Regions (region) VALUES ('Boise');
+INSERT INTO Regions (region) VALUES ('Boston');
+INSERT INTO Regions (region) VALUES ('BuffaloRochester');
+INSERT INTO Regions (region) VALUES ('California');
 
-INSERT INTO Months (month) VALUES ('12')
-INSERT INTO Months (month) VALUES ('11')
-INSERT INTO Months (month) VALUES ('10')
-INSERT INTO Months (month) VALUES ('09')
-INSERT INTO Months (month) VALUES ('08')
+INSERT INTO Months (month) VALUES ('12');
+INSERT INTO Months (month) VALUES ('11');
+INSERT INTO Months (month) VALUES ('10');
+INSERT INTO Months (month) VALUES ('09');
+INSERT INTO Months (month) VALUES ('08');
 
-INSERT INTO Prices (averageprice) VALUES ('1.33')
-INSERT INTO Prices (averageprice) VALUES ('1.35')
-INSERT INTO Prices (averageprice) VALUES ('0.93')
-INSERT INTO Prices (averageprice) VALUES ('1.02')
-INSERT INTO Prices (averageprice) VALUES ('1.00')
-INSERT INTO Prices (averageprice) VALUES ('0.88')
-INSERT INTO Prices (averageprice) VALUES ('0.92')
-INSERT INTO Prices (averageprice) VALUES ('1.11')
-INSERT INTO Prices (averageprice) VALUES ('1.03')
-INSERT INTO Prices (averageprice) VALUES ('1.23')
-INSERT INTO Prices (averageprice) VALUES ('0.99')
-INSERT INTO Prices (averageprice) VALUES ('1.21')
+INSERT INTO Prices (averageprice) VALUES ('1,33');
+INSERT INTO Prices (averageprice) VALUES ('1,35');
+INSERT INTO Prices (averageprice) VALUES ('0,93');
+INSERT INTO Prices (averageprice) VALUES ('1,02');
+INSERT INTO Prices (averageprice) VALUES ('1,00');
+INSERT INTO Prices (averageprice) VALUES ('0,88');
+INSERT INTO Prices (averageprice) VALUES ('0,92');
+INSERT INTO Prices (averageprice) VALUES ('1,11');
+INSERT INTO Prices (averageprice) VALUES ('1,03');
+INSERT INTO Prices (averageprice) VALUES ('1,23');
+INSERT INTO Prices (averageprice) VALUES ('0,99');
+INSERT INTO Prices (averageprice) VALUES ('1,21');
 
