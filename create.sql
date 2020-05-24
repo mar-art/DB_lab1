2a8039b0591bd37bc2bf06d600dbe7496120630f
mar-art <53080227+mar-art@users.noreply.github.com>
2020-05-24 12:42:34 +0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,27 +1,20 @@
 CREATE TABLE Avocado(
-id char(10) NOT NULL,
-averageprice float(10) NULL,
-region char(20) NULL,
-month char(10) NULL
+id NUMBER NOT NULL PRIMARY KEY
 );
 
 CREATE TABLE Prices(
-averageprice float(10) NOT NULL);
+id NUMBER NOT NULL PRIMARY KEY,
+avgprice NUMBER NOT NULL);
 
 CREATE TABLE Months(
+id NUMBER NOT NULL PRIMARY KEY,
 month char(10) NOT NULL);
 
 CREATE TABLE Regions(
+id NUMBER NOT NULL PRIMARY KEY,
 region char(20) NOT NULL);
 
-ALTER TABLE Avocado ADD CONSTRAINT id_pk PRIMARY KEY (id);
-ALTER TABLE Prices ADD CONSTRAINT averageprice_pk PRIMARY KEY (averageprice);
-ALTER TABLE Months ADD CONSTRAINT month_pk PRIMARY KEY (month);
-ALTER TABLE Regions ADD CONSTRAINT region_pk PRIMARY KEY (region);
 
-ALTER TABLE Avocado
-ADD CONSTRAINT averageprice_fk FOREIGN KEY (averageprice) REFERENCES Prices (averageprice);
-ALTER TABLE Avocado
-ADD CONSTRAINT month_fk FOREIGN KEY (month) REFERENCES Months (month);
-ALTER TABLE Avocado
-ADD CONSTRAINT region_fk FOREIGN KEY (region) REFERENCES Regions (region);
+ALTER TABLE Prices ADD CONSTRAINT price_fk FOREIGN KEY(id) REFERENCES Avocado(id);
+ALTER TABLE Months ADD CONSTRAINT month_fk FOREIGN KEY(id) REFERENCES Avocado(id);
+ALTER TABLE Regions ADD CONSTRAINT region_fk FOREIGN KEY(id) REFERENCES Avocado(id);
